d9da7781ad847138f9f8804dbfa2d44a644844ca
sumit pandit <its.sumitpandit@gmail.com>
2025-03-25 20:39:06 +0530

    sitemap added

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,20 @@ import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(),]
+  integrations: [
+    react(), 
+    sitemap(
+      {
+        changefreq: 'daily',
+      priority: 1.0,
+      lastmod: new Date(),
+      entryLimit: 1000,
+
+      }
+    )
+  ]
 });
